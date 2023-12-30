74495f004e8cb3223287111e717c15e06984c5c1
shanmukhavani <cskvani19@gmail.com>
2023-12-30 11:56:44 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [city,setCity] = useState('');
+  const [temp,setTemp] =useState('');
+  const submitHandler = (e)=>{
+    e.preventDefault();
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d885aa1d783fd13a55050afeef620fcb`).then(
+      response=>response.json()
+    ).then(
+      data=>{
+        const kelvin =data.main.temp;
+        // console.log(kelvin);
+        const celcius =kelvin-273.15;
+        setTemp("Temperature at "+city +" is \n "+Math.round(celcius)+"ºC" );
+        //setTemp(Math.round(celcius));
+      }
+    )
+      
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='nav-bar'>
+          <div className='logo'>
+
+          </div>
+            <h1>WEATHER APP</h1>
+            <form onSubmit={submitHandler}>
+               <input type='text' value={city} onChange={(e)=>{setCity(e.target.value)}}/>
+               <button type='submit'>Search</button>
+            </form>
+            
+      </div>
+      <p>
+        You can find temperature of any city here...
+        <h5 className='result'>{temp}</h5>
+        
+      </p>
+      
+      <div>
+      
+      </div>
+      
     </div>
   );
 }
